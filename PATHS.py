5be77906bcf156eb415e342579a343d4057b33a9
sankeerthana14 <sankeerthana.satini3@gmail.com>
2026-08-24 06:23:26 +0800

    adding gitignore and PATHS file

---
+++ PATHS.py
@@ -0,0 +1,11 @@
+import os
+
+CWD = os.getcwd()
+
+# MAIN DIRECTORIES
+DATA_DIR = os.path.join(CWD, "data")
+DATASETS_DIR = os.path.join(CWD, "datasets")
+MODELS_DIR = os.path.join(CWD, "models")
+NOTEBOOKS_DIR = os.path.join(CWD, "notebooks")
+SCRIPTS_DIR = os.path.join(CWD, "scripts")
+
